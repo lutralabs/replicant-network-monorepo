ed1070b23a85f638a4ca715a7f4c94181a89aa16
Urban Vidovič <urbanfoundit@gmail.com>
2025-03-06 21:37:00 +0100

    chore: update envio url
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- apps/dapp/src/config.ts
+++ apps/dapp/src/config.ts
@@ -1,10 +1,10 @@
 export const CONFIG = {
   mainnet: {
     contractAddress: '0x0000000000000000000000000000000000000000',
-    graphqlUrl: 'https://indexer.dev.hyperindex.xyz/b906ee3/v1/graphql',
+    graphqlUrl: '',
   },
   testnet: {
     contractAddress: '0xC70E380E0f2e8edE495C94E696b279778307b182',
-    graphqlUrl: 'https://indexer.dev.hyperindex.xyz/b906ee3/v1/graphql',
+    graphqlUrl: 'https://indexer.dev.hyperindex.xyz/f8b0d3f/v1/graphql',
   },
 };
