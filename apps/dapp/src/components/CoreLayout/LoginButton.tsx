a56b2d3b6710d38e47449a6b985a393811c7f381
martines3000 <domajnko.martin@gmail.com>
2025-03-10 17:02:07 +0100

    fix: improve address in navbar

--- apps/dapp/src/components/CoreLayout/LoginButton.tsx
+++ apps/dapp/src/components/CoreLayout/LoginButton.tsx
@@ -5,9 +5,10 @@ import {
 } from '@privy-io/react-auth';
 
 import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
 
 export function LoginButton() {
-  const { ready, authenticated, login, logout, user } = usePrivy();
+  const { ready, authenticated, login, logout } = usePrivy();
   const { wallets } = useWallets();
   // Disable login when Privy is not ready or the user is already authenticated
   const disableLogin = !ready || (ready && authenticated);
@@ -20,8 +21,18 @@ export function LoginButton() {
   if (ready && authenticated && wallet) {
     const chainId = wallet.chainId;
     return (
-      <div>
-        {wallet.address}
+      <div className="flex items-center gap-x-2">
+        <div
+          className={cn(
+            'inline-flex items-center rounded-full bg-gradient-to-r from-violet-500 to-indigo-500 px-4 py-1.5 text-sm font-medium text-white shadow-md'
+          )}
+        >
+          <div className="flex items-center gap-1.5">
+            <span className="font-mono">{wallet.address.slice(0, 6)}</span>
+            <span className="text-violet-300">•••</span>
+            <span className="font-mono">{wallet.address.slice(-4)}</span>
+          </div>
+        </div>
         {chainId !== 'eip155:10143' && (
           <Button
             variant="secondary"
@@ -32,7 +43,9 @@ export function LoginButton() {
             Wrong Chain
           </Button>
         )}
-        <Button onClick={logout}>Logout</Button>
+        <Button size="sm" onClick={logout}>
+          Logout
+        </Button>
       </div>
     );
   }
